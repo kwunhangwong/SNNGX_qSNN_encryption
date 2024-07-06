5bd6fb6fc5c780e5476bb44b4fdf3aa01995e2cf
Wong, Kwun Hang <56315946+u3556440@users.noreply.github.com>
2024-07-06 14:50:35 +0800

    Update __terminal__.sh

--- Encryption_SNNGX/__terminal__.sh
+++ Encryption_SNNGX/__terminal__.sh
@@ -5,13 +5,7 @@
 python3 __main__.py --epsil 50 --subset 16 --mutate 0.05 --qbit 8 --gen 125 -b 16 --seed 70\
     --Dataset 'NMNIST' --Dpath '/home/edwin/code/dataset/' --by_layer --layer_idx 1
 
-python3 __main__.py --epsil 50 --subset 16 --mutate 0.05 --qbit 8 --gen 125 -b 16 --seed 70\
-    --Dataset 'NMNIST' --Dpath '/home/edwin/code/dataset/' --by_layer --layer_idx 1
-
 # DVS_Gesture 
 # Input Layer: Layer_idx=0
-python3 __main__.py --epsil 30 --subset 16 --mutate 0.05 --qbit 8 --gen 95 -b 16 --seed 70\
-    --Dataset 'DVS128_Gesture' --Dpath '/home/edwin/code/dataset/' --by_layer --layer_idx 0
-
-python3 __main__.py --epsil 30 --subset 16 --mutate 0.05 --qbit 8 --gen 95 -b 16 --seed 70 \
-    --Dataset 'DVS128_Gesture' --Dpath '/home/edwin/code/dataset/' --by_layer --layer_idx 0
+# python3 __main__.py --epsil 30 --subset 16 --mutate 0.05 --qbit 8 --gen 95 -b 16 --seed 70\
+#     --Dataset 'DVS128_Gesture' --Dpath '/home/edwin/code/dataset/' --by_layer --layer_idx 0
